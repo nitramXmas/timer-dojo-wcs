357a59d17577bb12bc976de35faf9afa8f528860
nitramXmas <24914535+nitramXmas@users.noreply.github.com>
2023-03-13 17:28:18 +0100

    ajout fonction add users and few tests

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-let startBtn = document.querySelector('#btn-start'); //bouton de demarrage
+const startButton = document.querySelector('#btn-start'); //bouton de demarrage
+const addButton = document.querySelector('#btn-add'); //bouton d'ajout utilisateur
 let overlay = document.querySelector('.overlay'); //div avec la classe overlay
 let counterView = document.querySelector('#counter'); //la div du compteur
+let users = document.querySelector('.users'); //la div contenant les users
+const firstUser = document.querySelector('.input-user'); //selection de l'input de nom d'utilisateur
+const firstColor = document.querySelector('.input-color'); //selection de l'input de nom d'utilisateur
 
 //Selection de tous les inputs -> a ameliorer avec um object contenant le couple user/color
 // a ameliorer avec un bouton "+" pour ajouter une ligne de saisie
-let users = document.querySelectorAll('.input-user');
-let colors = document.querySelectorAll('.input-color');
+/* let users = document.querySelectorAll('.input-user');
+let colors = document.querySelectorAll('.input-color'); */
+
+/**
+ * Fonction ajout d'utilisateur
+ */
+addButton.addEventListener('click', function () {
+  let newUser = document.createElement('input');
+  newUser = firstUser;
+  console.log(newUser);
+  users.appendChild(newUser);
+});
 
 /**
  * Fonction daffichage de l'overlay au clic sur le bouton vert
  */
 
-startBtn.addEventListener('click', function () {
+startButton.addEventListener('click', function () {
   overlay.style.display = 'flex';
   //modification du contenu avec l'utilisateur et la couleur de fond -> a ameliorer avec un objets
   counterView.innerText = `${users[0].value} is coding`;
