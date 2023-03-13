46fb17d4d5af82acc221162bcd8d885cae7420f9
nitramXmas <24914535+nitramXmas@users.noreply.github.com>
2023-03-13 22:47:17 +0100

    delete user function v1

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const startButton = document.querySelector('#btn-start'); //bouton de demarrage
 const addButton = document.querySelector('#btn-add'); //bouton d'ajout utilisateur
+const delButton = document.querySelector('#btn-del'); //bouton d'ajout utilisateur
 let overlay = document.querySelector('.overlay'); //div avec la classe overlay
 let counterView = document.querySelector('#counter'); //la div du compteur
-let users = document.querySelector('.users'); //la div contenant les users
-const firstUser = document.querySelector('.input-user'); //selection de l'input de nom d'utilisateur
-const firstColor = document.querySelector('.input-color'); //selection de l'input de nom d'utilisateur
+let inputParam = document.querySelector('.input-param'); //la div contenant les inputs user et color
+const firstUser = document.querySelector('.input-user'); //selection du premier input user
+const firstColor = document.querySelector('.input-color'); //selection du premier input color
 
 //Selection de tous les inputs -> a ameliorer avec um object contenant le couple user/color
-// a ameliorer avec un bouton "+" pour ajouter une ligne de saisie
-/* let users = document.querySelectorAll('.input-user');
-let colors = document.querySelectorAll('.input-color'); */
 
 /**
  * Fonction ajout d'utilisateur
@@ -17,19 +15,38 @@ let colors = document.querySelectorAll('.input-color'); */
 addButton.addEventListener('click', function () {
   let newUser = document.createElement('input');
   let newColor = document.createElement('input');
-  users.appendChild(newUser);
-  users.appendChild(newColor);
+  newUser = firstUser.cloneNode(true);
+  newColor = firstColor.cloneNode(true);
+  inputParam.appendChild(newUser);
+  inputParam.appendChild(newColor);
 });
 
 /**
- * Fonction daffichage de l'overlay au clic sur le bouton vert
+ * Fonction supression d'utilisateur
+ * ameliorable avec objet ? + declanche une erreur dans la console
+ */
+
+delButton.addEventListener('click', function () {
+  let users = document.querySelectorAll('.input-user');
+  let colors = document.querySelectorAll('.input-color');
+  for (let cptDel = users.length - 1; cptDel > 0; cptDel++) {
+    users[cptDel].remove();
+    colors[cptDel].remove();
+  }
+});
+
+console.log(document.querySelectorAll('.input-user'));
+console.log(document.querySelectorAll('.input-color'));
+
+/**
+ * Fonction daffichage de l'overlay au clic sur le bouton de demarrage
  */
 
 startButton.addEventListener('click', function () {
   overlay.style.display = 'flex';
   //modification du contenu avec l'utilisateur et la couleur de fond -> a ameliorer avec un objets
-  counterView.innerText = `${users[0].value} is coding`;
-  counterView.style.backgroundColor = colors[0].value;
+  /*   counterView.innerText = `${users[0].value} is coding`;
+  counterView.style.backgroundColor = colors[0].value; */
 });
 
 //Sortie de l'overlay si on clique dessus - a modifier avec un bouton d'exit
